d57f18b7ef671c59817d34e57a4497538c358cc1
adensahwaludin <sekha1211@gmail.com>
2025-05-21 11:37:05 +0700

    Learn: v-for

--- resources/js/pages/Todolist.vue
+++ resources/js/pages/Todolist.vue
@@ -1,7 +1,7 @@
 <script setup>
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 
 const breadcrumbs = ref([
     {
@@ -9,33 +9,24 @@ const breadcrumbs = ref([
         href: '/belajar',
     },
     {
-        title: 'Score',
+        title: 'ToDoList',
         href: '/belajar/score',
     },
 ]);
-
-const state = ref({});
-
-watch(
-    () => state.value.score,
-    (newScore) => {
-        if (newScore > 100) {
-            state.value.message = 'Nilai tidak boleh lebih dari 100';
-        } else if (newScore < 0) {
-            state.value.message = 'Nilai tidak boleh kurang dari 0';
-        } else if (newScore >= 81) {
-            state.value.message = 'A';
-        } else if (newScore >= 61) {
-            state.value.message = 'B';
-        } else if (newScore >= 41) {
-            state.value.message = 'C';
-        } else if (newScore >= 21) {
-            state.value.message = 'D';
-        } else {
-            state.value.message = 'E';
-        }
+const items = [
+    {
+        id: 1,
+        name: 'Belajar Vue.js',
     },
-);
+    {
+        id: 2,
+        name: 'Belajar Laravel',
+    },
+    {
+        id: 3,
+        name: 'Belajar Inertia.js',
+    },
+];
 </script>
 
 <template>
@@ -46,21 +37,16 @@ watch(
                 <!-- Left column (50%) -->
                 <div class="w-full p-2 md:w-1/2">
                     <div class="flex flex-col gap-4">
-                        <input
-                            type="number"
-                            v-model="state.score"
-                            class="w-full rounded-md border-2 border-gray-300 p-2"
-                            placeholder="Masukkan Nilai"
-                            max="100"
-                        />
-                        <div v-if="!state.score">Silahkan isi nilai kamu</div>
-                        <!-- ini akan menjadi hidden -->
-                        <div v-else>Nilai angka kamu {{ state.score }} kamu dapat nilai {{ state.message }}</div>
+                        <ul v-for="item in items">
+                            <li v-for="(value, key) in item">{{ key }} : {{ value }}</li>
+                        </ul>
                     </div>
                 </div>
                 <!-- Right column (50%) -->
                 <div class="w-full p-2 md:w-1/2">
-                    <div class="flex flex-col gap-4"></div>
+                    <div class="flex flex-col gap-4">
+                        <h1 v-for="n in 10">Angka {{ n }}</h1>
+                    </div>
                 </div>
             </div>
         </div>
